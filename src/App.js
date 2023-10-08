a6ed9324851aa8269982a0c5626ea8bc7e824a43
zahoorshinwari <khanzshinwari5371@gmail.com>
2023-10-08 11:34:42 +0500

    updated the dropdown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 
 function App() {
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+
     const options = [
-        { label: 'Red', valu: 'red'},
-        { label: 'Green', valu: 'green'},
-        { label: 'Blue', valu: 'blue'},
+        { label: 'Red', value: 'red'},
+        { label: 'Green', value: 'green'},
+        { label: 'Blue', value: 'blue'},
     ];
 
      return (
         <div>
-            <Dropdown options={options}/>  
+            <Dropdown 
+                options={options} 
+                selection={selection} 
+                onSelect={handleSelect}/>  
         </div>
      ) 
      
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function Dropdown({ options }) {
+function Dropdown({ options , selection, onSelect }) {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = () => {
@@ -18,15 +18,21 @@ function Dropdown({ options }) {
         setIsOpen(false);
 
         // what option did the user click on.
-        console.log(option);
+        onSelect(option);
     }
  
     const renderedOptions = options.map((option) => {
         return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>});
     
+
+
         return (
         <div>
-            <div onClick={handleClick}>Select...</div>
+            <div onClick={handleClick}>
+            
+                {/* the below statment mean that if the selection is null then show the 2nd option. while if the selection is not null then show the selection?.label that are selected */}
+                {selection?.label || 'Select....'}</div>
+            
             {/* if is open is is true then show the rendered option if false the can't show */}
             {isOpen && <div>{renderedOptions}</div>}
         </div>
