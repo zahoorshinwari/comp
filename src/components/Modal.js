864256d840e97d0083bd43e6b5f52c3c7ea932bb
zahoorshinwari <khanzshinwari5371@gmail.com>
2023-10-12 00:25:53 +0500

    updated

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,8 +17,8 @@ function Modal( {onClose, children, actionBar} ) {
 
     return ReactDOM.createPortal(
         <div>
-            <div onClick={onClose} className='absolute inset-0 bg-gray-300 opacity-80'></div>
-            <div className='absolute inset-40 p-10 bg-white'>
+            <div onClick={onClose} className='fixed inset-0 bg-gray-300 opacity-80'></div>
+            <div className='fixed inset-40 p-10 bg-white'>
                 <div className='flex flex-col justify-between h-full'>
                     {children}
                     <div className='flex justify-end '>
