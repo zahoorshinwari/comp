36229f675336ff0ce49e427783019ee2da36dc4e
zahoorshinwari <khanzshinwari5371@gmail.com>
2023-10-15 11:18:05 +0500

    updated the counter page and add the form to add some values to counter

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,19 +1,58 @@
 import Button from '../components/Button'
-import useCounter from '../hooks/use-counter'
-
+import { useState } from 'react'
+import Panel from '../components/Panel'
 
 
 function CounterPage ( { initialCount }){
-    // in the components, call the hook function and destructure the properties that the components needed
-    const { count , increment } = useCounter(initialCount)
+    
+    const [ count, setCount ] = useState(initialCount)
 
-    return <div>
-        <h1>Count is {count}</h1>
+    const [ valueToAdd, setValueToAdd] = useState(0)
 
-        <Button onClick={increment}>
-            Increement
-        </Button>
-    </div>
+
+    const increment = () => {
+        setCount( count + 1)
+    }
+
+    const decrement = () => {
+        setCount( count - 1)
+    }
+    
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0
+
+        setValueToAdd(value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        setCount( count + valueToAdd)
+        setValueToAdd(0)
+    }
+
+    return (
+        <Panel className='m-3'>
+            <h1 className='text-lg'>Count is :{count}</h1>
+            <div className='flex flex-row'>
+                <Button onClick={increment}>Increement</Button>
+                <Button onClick={decrement}>Decrement</Button>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input 
+                    type='number' 
+                    className='p-1 m-3 bg-gray-50 border border-gray-300'
+                    value={valueToAdd || ''}
+                    onChange={handleChange}
+                />
+
+                <Button>Add it!</Button>
+
+            </form>
+        </Panel>
+    )
 
 }
 
